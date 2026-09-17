5d67316898aa4b57d15d7374448e500cc2809f03
Deepanshu Singh <deevyanshusingh123@gmail.com>
2026-09-17 23:05:25 +0530

    Register API

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,16 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -4,7 +4,7 @@ const userSchema=new mongoose.Schema({
 
 name:{
     type:String,
-    reuired:true,
+    required:true,
     trim:true
 },
 
